bc6b85487577069fe5838319ccc40172d9b0ac10
alanher120 <34939103+alanher120@users.noreply.github.com>
2023-05-27 15:27:48 +0800

    Update ingress-nginx.sh
    change install ingress-nginx using user vagrant

--- scripts/ingress-nginx.sh
+++ scripts/ingress-nginx.sh
@@ -2,10 +2,10 @@
 set -euxo pipefail
 
 # install 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/v${INGRESS_NGINX_VERSION}/deploy/static/provider/baremetal/deploy.yaml
+sudo -i -u vagrant kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/v${INGRESS_NGINX_VERSION}/deploy/static/provider/baremetal/deploy.yaml
 
 # wait until is ready
-kubectl wait --namespace ingress-nginx \
+sudo -i -u vagrant kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=120s
