48b0850e8711e84950107b7ebfd46b81bc847151
alanher120 <34939103+alanher120@users.noreply.github.com>
2023-05-27 19:38:23 +0800

    Update master.sh
    fix rm -f $config_path/* issue

--- scripts/master.sh
+++ scripts/master.sh
@@ -12,6 +12,13 @@ sudo kubeadm config images pull
 echo "Preflight Check Passed: Downloaded All Required Images"
 
 if [ "$HOSTNAME" == "master-node" ];then
+  # For Vagrant re-runs, check if there is existing configs in the location and delete it for saving new configuration.
+  if [ -d $config_path ]; then
+    rm -f $config_path/*
+  else
+    mkdir -p $config_path
+  fi
+  
   # runing on first master
   sudo kubeadm init --apiserver-advertise-address=$CONTROL_IP --apiserver-cert-extra-sans=$CONTROL_IP --pod-network-cidr=$POD_CIDR --service-cidr=$SERVICE_CIDR --node-name "$NODENAME" --ignore-preflight-errors Swap
   
@@ -28,14 +35,6 @@ sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
 # Save Configs to shared /Vagrant location
-
-# For Vagrant re-runs, check if there is existing configs in the location and delete it for saving new configuration.
-
-if [ -d $config_path ]; then
-  rm -f $config_path/*
-else
-  mkdir -p $config_path
-fi
 
 cp -i /etc/kubernetes/admin.conf $config_path/config
 touch $config_path/join.sh
