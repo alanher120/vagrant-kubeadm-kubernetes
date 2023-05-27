44b08473d4f809a9df52b15fc9d13e41f21d2308
alanher120 <34939103+alanher120@users.noreply.github.com>
2023-05-27 13:09:23 +0800

    Update dashboard.sh
    update add install metrics-server

--- scripts/dashboard.sh
+++ scripts/dashboard.sh
@@ -8,6 +8,7 @@ config_path="/vagrant/configs"
 
 DASHBOARD_VERSION=$(grep -E '^\s*dashboard:' /vagrant/settings.yaml | sed -E 's/[^:]+: *//')
 if [ -n "${DASHBOARD_VERSION}" ]; then
+  kubectl apply -f https://raw.githubusercontent.com/techiescamp/kubeadm-scripts/main/manifests/metrics-server.yaml
   echo 'Waiting for metrics server to be ready...'
   kubectl wait --namespace kube-system \
     --for=condition=ready pod \
