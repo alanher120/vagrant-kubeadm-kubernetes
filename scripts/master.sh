8d806f8b79864fb7b098ec66c8c7bd7fc64becd6
alanher120 <34939103+alanher120@users.noreply.github.com>
2023-05-27 20:00:52 +0800

    Update master.sh
    fix non-master up error issue

--- scripts/master.sh
+++ scripts/master.sh
@@ -22,11 +22,25 @@ if [ "$HOSTNAME" == "master-node" ];then
   # runing on first master
   sudo kubeadm init --apiserver-advertise-address=$CONTROL_IP --apiserver-cert-extra-sans=$CONTROL_IP --pod-network-cidr=$POD_CIDR --service-cidr=$SERVICE_CIDR --node-name "$NODENAME" --ignore-preflight-errors Swap
   
-  kubeadm token create --print-join-command > $config_path/join.sh
+  # Install Calico Network Plugin
+  #curl https://raw.githubusercontent.com/projectcalico/calico/v${CALICO_VERSION}/manifests/calico.yaml -O
+  #kubectl apply -f calico.yaml
+  kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v${CALICO_VERSION}/manifests/calico.yaml
   
+  # Save Configs to shared /Vagrant location
+  cp -i /etc/kubernetes/admin.conf $config_path/config
+  
+  # create join.sh
+  kubeadm token create --print-join-command > $config_path/join.sh
+  touch $config_path/join.sh
+  chmod +x $config_path/join.sh
+  
+  # create master-join.sh
   kubeadm init phase upload-certs --upload-certs > $config_path/master-cert-key
   CERT_KEY=`tail -1 $config_path/master-cert-key`
   cat $config_path/join.sh|while read x;do echo "${x} --control-plane --certificate-key ${CERT_KEY}" ;done > $config_path/master-join.sh
+  touch $config_path/join.sh
+  chmod +x $config_path/join.sh
 else
   # runing on non-first master
   /bin/bash $config_path/master-join.sh -v 
@@ -35,20 +49,6 @@ fi
 mkdir -p "$HOME"/.kube
 sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
-
-# Save Configs to shared /Vagrant location
-
-cp -i /etc/kubernetes/admin.conf $config_path/config
-touch $config_path/join.sh
-chmod +x $config_path/join.sh
-
-# Install Calico Network Plugin
-
-#curl https://raw.githubusercontent.com/projectcalico/calico/v${CALICO_VERSION}/manifests/calico.yaml -O
-
-#kubectl apply -f calico.yaml
-
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v${CALICO_VERSION}/manifests/calico.yaml
 
 sudo -i -u vagrant bash << EOF
 whoami
