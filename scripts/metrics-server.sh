134ddb5d80212521fb14d0fa1a1aca4d55af20a8
alanher120 <34939103+alanher120@users.noreply.github.com>
2023-05-27 15:29:45 +0800

    Update metrics-server.sh
    change install metrics-server us user vagrant

--- scripts/metrics-server.sh
+++ scripts/metrics-server.sh
@@ -2,10 +2,10 @@
 set -euxo pipefail
 
 # Install Metrics Server
-kubectl apply -f https://raw.githubusercontent.com/techiescamp/kubeadm-scripts/main/manifests/metrics-server.yaml
+sudo -i -u vagrant kubectl apply -f https://raw.githubusercontent.com/techiescamp/kubeadm-scripts/main/manifests/metrics-server.yaml
 
 echo 'Waiting for metrics server to be ready...'
-kubectl wait --namespace kube-system \
+sudo -i -u vagrant kubectl wait --namespace kube-system \
   --for=condition=ready pod \
   --selector=k8s-app=metrics-server \
   --timeout=120s
