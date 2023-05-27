b3a327b5adeac34ce72bbe708930689af967b4f6
alanher120 <34939103+alanher120@users.noreply.github.com>
2023-05-27 10:31:33 +0800

    Create ingress-nginx.sh
    create ingress-nginx.sh

---
+++ scripts/ingress-nginx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euxo pipefail
+
+# install 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/v{$INGRESS_NGINX_VERSION}/deploy/static/provider/baremetal/deploy.yaml
+
+# wait until is ready
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=120s
+
+echo "ingress-nginx installed"
