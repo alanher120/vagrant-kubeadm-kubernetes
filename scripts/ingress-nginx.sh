f58b7d793e95190c68ae7e4aadecbdb2573a62c0
alanher120 <34939103+alanher120@users.noreply.github.com>
2023-05-28 13:08:16 +0800

    Update ingress-nginx.sh

--- scripts/ingress-nginx.sh
+++ scripts/ingress-nginx.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -euxo pipefail
 
-# install 
-#sudo -i -u vagrant kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/v${INGRESS_NGINX_VERSION}/deploy/static/provider/baremetal/deploy.yaml
-if [ "${INGRESS_NGINX_VERSION}" == "main" ];then
-  sudo -i -u vagrant kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/baremetal/deploy.yaml
-else
-  sudo -i -u vagrant kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/v${INGRESS_NGINX_VERSION}/deploy/static/provider/baremetal/deploy.yaml
+if [ -n "${INGRESS_NGINX_VERSION}" ]; then
+  # install 
+  #sudo -i -u vagrant kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/v${INGRESS_NGINX_VERSION}/deploy/static/provider/baremetal/deploy.yaml
+  if [ "${INGRESS_NGINX_VERSION}" == "main" ];then
+    sudo -i -u vagrant kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/baremetal/deploy.yaml
+  else
+    sudo -i -u vagrant kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/v${INGRESS_NGINX_VERSION}/deploy/static/provider/baremetal/deploy.yaml
+  fi
+
+  # wait until is ready
+  sudo -i -u vagrant kubectl wait --namespace ingress-nginx \
+    --for=condition=ready pod \
+    --selector=app.kubernetes.io/component=controller \
+    --timeout=180s
+
+  echo "ingress-nginx installed"
 fi
-
-# wait until is ready
-sudo -i -u vagrant kubectl wait --namespace ingress-nginx \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/component=controller \
-  --timeout=180s
-
-echo "ingress-nginx installed"
