10cab3562a9e4571cd43e9f101f7821cacb804f5
alanher120 <34939103+alanher120@users.noreply.github.com>
2023-05-28 14:02:02 +0800

    Update flannel.sh

--- scripts/flannel.sh
+++ scripts/flannel.sh
@@ -10,6 +10,14 @@ if [ -n "calico" ];then
     KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f https://github.com/flannel-io/flannel/releases/laeset/download/kube-flannel.yml
   fi
   
+cat <<EOF | KUBECONFIG=/etc/kubernetes/admin.conf kubectl -n kube-flannel patch cm patch-file -
+data:
+  net-conf.json: |
+    {
+      "Network": "$POD_CIDR"
+    }
+EOF
+  
   sleep 20
   KUBECONFIG=/etc/kubernetes/admin.conf kubectl wait --namespace kube-system \
                 --for=condition=ready pod \
