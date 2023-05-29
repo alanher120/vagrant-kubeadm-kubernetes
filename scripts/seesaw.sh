d4208a153a5519c523e3bd26edcc37300f43b324
alanher120 <34939103+alanher120@users.noreply.github.com>
2023-05-29 20:04:07 +0800

    Update seesaw.sh

--- scripts/seesaw.sh
+++ scripts/seesaw.sh
@@ -90,3 +90,83 @@ install "etc/seesaw/watchdog.cfg" "${SEESAW_ETC}"
 # Enable CAP_NET_RAW for seesaw binaries that require raw sockets.
 /sbin/setcap cap_net_raw+ep "${SEESAW_BIN}/seesaw_ha"
 /sbin/setcap cap_net_raw+ep "${SEESAW_BIN}/seesaw_healthcheck"
+
+# write seesaw.cfg
+cat <<EOF > /etc/seesaw/seesaw.cfg
+[cluster]
+anycast_enabled = false
+name = au-syd
+node_ipv4 = 10.0.0.10
+peer_ipv4 = 10.0.0.15
+vip_ipv4 = 10.0.0.9
+
+[config_server]
+primary = master-node
+secondary = worker-node01
+tertiary = worker-node02
+
+[interface]
+node = eth1
+lb = eth2
+EOF
+
+# write cluster.pb
+cat <<EOF > /etc/seesaw/cluster.pb
+seesaw_vip: <
+  fqdn: "seesaw-vip1.example.com."
+  ipv4: "192.168.10.1/24"
+  status: PRODUCTION
+>
+node: <
+  fqdn: "seesaw1-1.example.com."
+  ipv4: "192.168.10.2/24"
+  status: PRODUCTION
+>
+node: <
+  fqdn: "seesaw1-2.example.com."
+  ipv4: "192.168.10.3/24"
+  status: PRODUCTION
+>
+vserver: <
+  name: "dns.resolver@au-syd"
+  entry_address: <
+    fqdn: "dns-anycast.example.com."
+    ipv4: "10.0.0.5/24"
+    status: PRODUCTION
+  >
+  rp: "corpdns-team@example.com"
+  vserver_entry: <
+    protocol: TCP
+    port: 53
+    scheduler: RR
+    server_low_watermark: 0.3
+    healthcheck: <
+      type: DNS
+      interval: 5
+      timeout: 2
+      port: 53
+      send: "www.example.com"
+      receive: "10.0.0.5"
+      mode: DSR
+      method: "a"
+      retries: 1
+    >
+  >
+  backend: <
+    host: <
+      fqdn: "worker-node01."
+      ipv4: "10.0.0.15/24"
+      status: PRODUCTION
+    >
+    weight: 1
+  >
+  backend: <
+    host: <
+      fqdn: "worker-node02."
+      ipv4: "10.0.0.16/24"
+      status: PRODUCTION
+    >
+    weight: 1
+  >
+>
+EOF
