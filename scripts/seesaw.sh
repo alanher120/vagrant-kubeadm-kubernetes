2c66a7dd18b4e863c5ca4231c5706a24ed994088
alanher120 <34939103+alanher120@users.noreply.github.com>
2023-05-29 11:42:40 +0800

    Update seesaw.sh

--- scripts/seesaw.sh
+++ scripts/seesaw.sh
@@ -2,6 +2,25 @@
 # https://codeantenna.com/a/FOtutjP9od
 set -euxo pipefail
 
+modprobe ip_vs
+modprobe nf_conntrack_ipv4
+modprobe dummy numdummies=1
+
+# modprobe.d
+if [ -d /etc/modprobe.d/ ];then
+  echo options ip_vs > /etc/modprobe.d/ip_vs.conf
+  echo options nf_conntrack_ipv4 > /etc/modprobe.d/nf_conntrack_ipv4.conf
+  echo options dummy numdummies=1 > /etc/modprobe.d/dummy.conf
+fi
+
+# systemd
+if [ -d /etc/modules.load.d/ ];then
+  echo ip_vs > /etc/modules.load.d/ip_vs.conf
+  echo nf_conntrack_ipv4 > /etc/modules.load.d/nf_conntrack_ipv4.conf
+  echo dummy numdummies=1 > /etc/modules.load.d/dummy.conf
+  systemctl restart systemd-modules-load.service
+fi
+
 SEESAW_BIN="/usr/local/seesaw"
 SEESAW_ETC="/etc/seesaw"
 SEESAW_LOG="/var/log/seesaw"
